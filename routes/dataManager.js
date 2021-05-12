ff9fd4c08200b988c9d4e5d0728ad339b6864045
Devashish Gupta <deva.sahab.25@gmail.com>
2021-05-12 16:27:40 +0530

    added webhooks

--- routes/dataManager.js
+++ routes/dataManager.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { setupWorkspace } = require('../controller/DataManager/Setup')
-
+const { updateCustomer, updateOrder, updateProduct } = require("../controller/ShopifyManager/Webhooks/index");
 
 router.post('/data-manager/setup', async (req, res) => {
     const { workspaceId } = req.body
@@ -10,22 +10,28 @@ router.post('/data-manager/setup', async (req, res) => {
 })
 
 router.post('/data-manager/customer/add', (req, res) => {
+    const {customer} = req.body;
+    const workspaceId = 1;
+    const response = await updateCustomer(customer, workspaceId);
     //delete and insert customer of that id
-
-    res.status(200).send("")
+    res.status(200).send(response);
 })
 
 
 router.post('/data-manager/order/add', (req, res) => {
+    const {order} = req.body;
+    const workspaceId = 1;
     //delete and insert order of that id, all lineitems of that order, all fufilment of that order
-
-    res.status(200).send("")
+    const response = await updateOrder(order, workspaceId);
+    res.status(200).send(response);
 })
 
 router.post('/data-manager/product/add', (req, res) => {
-    //delete and insert product of that id, all vairant of that order
-    
-    res.status(200).send("")
+    const {product} = req.body;
+    const workspaceId = 1;
+    //delete and insert product of that id, all variant of that order
+    const response = await updateProduct(product, workspaceId);
+    res.status(200).send(response);
 })
 
 
