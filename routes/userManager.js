2cfbe9d79fc41f85fb4343a12f5713ea394637c4
shimin8 <nimish007gupta@gmail.com>
2021-05-20 12:57:44 +0530

    api routes

--- routes/userManager.js
+++ routes/userManager.js
@@ -1,143 +1,78 @@
 const express = require('express')
-const { insert, del, fetch, fetchAll } = require('../aws/index.js')
+const {
+    createUser, 
+    createWorkspace, 
+    createUserToWorkspce,
+
+    addUser,
+    addWorkspace,
+    addUserToWorkspace,
+
+
+    deleteUser,
+    deleteWorkspace,
+    deleteUserToWorkspace,
+
+    fetchUser, 
+    fetchWorkspace,
+    fetchAllUsers,   //params workspaceid
+    fetchAllWorkspaces //parms user id 
+} = require('../controller/UserManager/index.js')
+
 const router = express.Router()
 
 /***** ADD *****/
 router.post('/user-manager/user/add', async function (req, res) {
-    let { name, email, password } = req.body
-    let params = {
-        TableName: "User",
-        Item: {
-            name,
-            email,
-            password,
-            user_id: Date.now()+''
-        }
-    }
-    let response = await insert(params)
+    let response = await addUser(params)
     // let response = await insert("User", req.body)
     return res.status(200).send(response)
 })
 
 router.post('/user-manager/user-to-workspace/add', async function (req, res) {
-    let { user_id, workspace_id } = req.body
-    let params = {
-        TableName: "UserToWorkspace",
-        Item: {
-            id: Date.now(),
-            user_id,
-            workspace_id
-        }
-    }
-    let response = await insert(params)
+    let response = await addUserToWorkspace(params)
     res.status(200).send(response)
 })
 
 router.post('/user-manager/workspace/add', function (req, res) {
-    let { workspace_name } = req.body
-    let params = {
-        TableName: "Workspace",
-        Item: {
-            workspace_id: Date.now(),
-            workspace_name
-        }
-    }
-    let response = await insert(params)
+    let response = await addWorkspace(params)
     res.status(200).send(response)
 })
 
 /***** DELETE *****/
 router.post('/user-manager/user/delete', async function (req, res) {
-    // console.log(req.body)
-    let { user_id } = req.body
-    let params = {
-        TableName: "User",
-        Key: {
-            user_id
-        }
-    }
-    console.log(params)
-    let response = await del(params)
-    // let response = await del("User", req.body)
+    let response = await deleteUser(params)
     res.status(200).send(response)
 })
 
 router.post('/user-manager/user-to-workspace/delete', function (req, res) {
-    let { id } = req.body
-    let params = {
-        TableName: "UserToWorkspace",
-        Key: {
-            id
-        }
-    }
-    console.log(params)
-    let response = await del(params)
+    let response = await deleteUserToWorkspace(params)
     res.status(200).send(response)
 })
 
 router.post('/user-manager/workspace/delete', function (req, res) {
-    let { workspace_id } = req.body
-    let params = {
-        TableName: "Workspace",
-        Key: {
-            workspace_id
-        }
-    }
-    console.log(params)
-    let response = await del(params)
+    let response = await deleteWorkspace(params)
     res.status(200).send(response)
 })
 
 /***** FETCH *****/
 router.post('/user-manager/user/fetch', function (req, res) {
-    let { user_id, name, email, password } = req.body
-    let params = {
-        TableName: "User",
-        Item: {
-            user_id,
-            name,
-            email,
-            password
-        }
-    }
-    console.log(params)
-    let response = await fetch(params)
+    let response = await fetchUser(params)
     res.status(200).send(response)
 })
 
 router.post('/user-manager/workspace/fetch', function (req, res) {
-    let { workspace_id, workspace_name } = req.body
-    let params = {
-        TableName: "Workspace",
-        Item: {
-            workspace_id,
-            workspace_name
-        }
-    }
-    console.log(params)
-    let response = await fetch(params)
+    let response = await fetchWorkspace(params)
     res.status(200).send(response)
 })
 
 /***** FETCHALL *****/
 router.post('/user-manager/user/fetchAll', function (req, res) {
-    let params = {
-        TableName: "User"
-    }
-    console.log(params)
-    let response = await fetchAll(params)
+    let response = await fetchAllUsers(params)
     res.status(200).send(response)
 })
 
 router.post('/user-manager/workspace/fetchAll', function (req, res) {
-    let params = {
-        TableName: "Workspace",
-        Key: {
-            workspace_id
-        }
-    }
-    console.log(params)
-    let response = await fetchAll(params)
+    let response = await fetchAllWorkspaces(params)
     res.status(200).send(response)
 })
 
