26b788b3eaca2ec740ee9daea4ec1c567f53f631
Devashish Gupta <Reptile2503@users.noreply.github.com>
2021-05-21 12:50:36 +0530

    fixed issue

--- Socket/socket.js
+++ Socket/socket.js
@@ -1,20 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http,{
-    cors: {
-        origin: "http://localhost:3000"
-    }
-});
+var io = require('socket.io')(http);
+
+const PORT = 4000;
 
 io.of("/workspace").on("connection", (socket) => {
     workspaceId = 1;
-    console.log(socket.id);
+    console.log("user connected");
     socket.join(workspaceId);
     socket.on("sync", (message) => {
         console.log(message);
     })
 })
 
-http.listen(3001, function() {
-   console.log('listening on localhost:3001');
+http.listen(PORT, function() {
+   console.log(`listening on localhost:${PORT}`);
 });
--- socket.js
+++ socket.js
@@ -1,13 +1,13 @@
 const io = require("socket.io-client");
-const socket = io("http://localhost:3001/workspace");
+const socket = io("http://localhost:4000/workspace");
 
 socket.on("connect", () => {
-    console.log("connected", socket.id);
+    console.log("connected");
 })
 
-socket.on("connect_error", (message) => {
-    console.log(message);
-})
+// socket.on("connect_error", (message) => {
+//     console.log(message);
+// })
 
 module.exports = {
     socket
