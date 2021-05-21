67bead6b61af4eb70adcd23421fadb47a31fe9ee
shimin8 <nimish007gupta@gmail.com>
2021-05-21 13:24:43 +0530

    barGraph, userManager api

--- routes/userManager.js
+++ routes/userManager.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const {
-    createUser, 
-    createWorkspace, 
-    createUserToWorkspce,
+    // createUser, 
+    // createWorkspace, 
+    // createUserToWorkspce,
 
     addUser,
     addWorkspace,
@@ -23,56 +23,56 @@ const router = express.Router()
 
 /***** ADD *****/
 router.post('/user-manager/user/add', async function (req, res) {
-    let response = await addUser(params)
+    let response = await addUser(req.body)
     // let response = await insert("User", req.body)
     return res.status(200).send(response)
 })
 
 router.post('/user-manager/user-to-workspace/add', async function (req, res) {
-    let response = await addUserToWorkspace(params)
+    let response = await addUserToWorkspace(req.body)
     res.status(200).send(response)
 })
 
-router.post('/user-manager/workspace/add', function (req, res) {
-    let response = await addWorkspace(params)
+router.post('/user-manager/workspace/add', async function (req, res) {
+    let response = await addWorkspace(req.body)
     res.status(200).send(response)
 })
 
 /***** DELETE *****/
 router.post('/user-manager/user/delete', async function (req, res) {
-    let response = await deleteUser(params)
+    let response = await deleteUser(req.body)
     res.status(200).send(response)
 })
 
-router.post('/user-manager/user-to-workspace/delete', function (req, res) {
-    let response = await deleteUserToWorkspace(params)
+router.post('/user-manager/user-to-workspace/delete', async function (req, res) {
+    let response = await deleteUserToWorkspace(req.body)
     res.status(200).send(response)
 })
 
-router.post('/user-manager/workspace/delete', function (req, res) {
-    let response = await deleteWorkspace(params)
+router.post('/user-manager/workspace/delete', async function (req, res) {
+    let response = await deleteWorkspace(req.body)
     res.status(200).send(response)
 })
 
 /***** FETCH *****/
-router.post('/user-manager/user/fetch', function (req, res) {
-    let response = await fetchUser(params)
+router.post('/user-manager/user/fetch', async function (req, res) {
+    let response = await fetchUser(req.body)
     res.status(200).send(response)
 })
 
-router.post('/user-manager/workspace/fetch', function (req, res) {
-    let response = await fetchWorkspace(params)
+router.post('/user-manager/workspace/fetch', async function (req, res) {
+    let response = await fetchWorkspace(req.body)
     res.status(200).send(response)
 })
 
-/***** FETCHALL *****/
-router.post('/user-manager/user/fetchAll', function (req, res) {
-    let response = await fetchAllUsers(params)
+/***** FETCH-ALL *****/
+router.post('/user-manager/user/fetchAll', async function (req, res) {
+    let response = await fetchAllUsers(req.body)
     res.status(200).send(response)
 })
 
-router.post('/user-manager/workspace/fetchAll', function (req, res) {
-    let response = await fetchAllWorkspaces(params)
+router.post('/user-manager/workspace/fetchAll', async function (req, res) {
+    let response = await fetchAllWorkspaces(req.body)
     res.status(200).send(response)
 })
 
