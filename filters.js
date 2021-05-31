0a34d9276742fa1b2c8c34096e7abc5ac9ae0d1f
shimin8 <nimish007gupta@gmail.com>
2021-05-31 17:35:40 +0530

    customer aggregate

--- filters.js
+++ filters.js
@@ -1,5 +1,21 @@
+const {
+    CUSTOMER_TABLE_NAME, 
+    ORDER_TABLE_NAME,
+    PRODUCT_TABLE_NAME,
+    DISCOUNT_TABLE_NAME,
+    FULFILLMENT_TABLE_NAME,
+    LINEITEMS_TABLE_NAME,
+    REFUNDED_TABLE_NAME,
+    VARIANT_TABLE_NAME,
+    CART_TABLE_NAME,
+    CARTLINEITEMS_TABLE_NAME,
+    CHECKOUT_TABLE_NAME,
+    CHECKOUTLINEITEMS_TABLE_NAME,
+    PAGEVIEWED_TABLE_NAME,
+    CUSTOMERAGGREGATE_TABLE_NAME
+} = require("./controller/DataManager/helper.js");
 
-let filters = {
+let filters1 = {
     "relation": "AND",  //AND  OR
     conditions: [
         {
@@ -12,8 +28,8 @@ let filters = {
                 values: ['2020-05-13 11:49:40.765997+05:30', '2021-05-13 11:49:40.765997+05:30']
             },
             {
-                type: "FULFILLMENT", //CUSTOMER. ORDER, Cart
-                columnName: "accepts_marketing",
+                type: "ORDER", //CUSTOMER. ORDER, Cart
+                columnName: "buyer_accepts_marketing",
                 filterType: "equal_to",  // >, <,=, not 
                 dataType: "boolean",
                 values: 'FALSE'
@@ -45,7 +61,7 @@ let filters = {
             },
             {
                 type: "CUSTOMER", //CUSTOMER. ORDER, Cart
-                columnName: "amount",
+                columnName: "total_spent",
                 filterType: "in",  // >, <,=, not 
                 dataType: "numeric[]",
                 values: [10, 20, 30]
@@ -102,46 +118,58 @@ let filters = {
 // "SELECT name FROM customers WHERE 
 // id IN (SELECT cid FROM orders WHERE date < DATE_SUB(CURDATE(), INTERVAL 30 DAY) GROUP BY(cid) HAVING COUNT(*) > 5)";
 
-const whereClause = (filters, ptype) => {
+const whereClause = (filters = filters1, workspaceId = 333, ptype) => {
     if(filters.conditions) {
         return `(${filters.conditions.map((filter, index) => {
             // console.log('!!!')
             // console.log(filter.type)
             if(!index) ptype = filter.type
-            return whereClause(filter, ptype)
+            return whereClause(filter, workspaceId, ptype)
         }).join(` ${ filters.relation } `)} )`
     } else {
-        return typeBuild(ptype, filters)
+        return typeBuild(ptype, workspaceId, filters)
     }
 }
 
-const typeBuild = (ptype, { columnName, filterType, dataType, values, type }) => {
+const typeBuild = (ptype, workspaceId, { columnName, filterType, dataType, values, type }) => {
     let prefix = ''
     console.log(ptype, type)
-    if(ptype === 'CUSTOMER' && ptype != type) {
-        prefix = `id IN (SELECT customer_id FROM ${type} WHERE `
-    } else if(ptype === 'ORDER' && ptype != type) {
-        prefix = `id IN (SELECT order_id FROM ${type} WHERE `
-    } else if(ptype === 'FULFILLMENT' && ptype != type) {
-        prefix = `id IN (SELECT fulfillment_id FROM ${type} WHERE `
-    } else if(ptype === 'PRODUCT' && ptype != type) {
-        prefix = `id IN (SELECT product_id FROM ${type} WHERE `
-    } else if(typeof ptype === 'undefined') {
-        prefix = `id IN (SELECT id FROM ${type} WHERE `
-        // if(type === 'CUSTOMER') {
-        //     prefix = `id IN (SELECT customer_id FROM ${type} WHERE `
-        // } else if(type === 'ORDER') {
-        //     prefix = `id IN (SELECT order_id FROM ${type} WHERE `
-        // } else if(type === 'FULFILLMENT') {
-        //     prefix = `id IN (SELECT fulfillment_id FROM ${type} WHERE `
-        // } else if(type === 'PRODUCT') {
-        //     prefix = `id IN (SELECT product_id FROM ${type} WHERE `
-        // }
+    let table = ''
+    if(type === 'CUSTOMER') {
+        table = CUSTOMER_TABLE_NAME(workspaceId)
+    } else if(type === 'ORDER') {
+        table = ORDER_TABLE_NAME(workspaceId)
+    } else if(type === 'PRODUCT') {
+        table = PRODUCT_TABLE_NAME(workspaceId)
+    } else if(type === 'FULFILLMENT') {
+        table = FULFILLMENT_TABLE_NAME(workspaceId)
+    } else {
+        return;
     }
 
-    // if(type != 'CUSTOMER') {
-    //     prefix = `id IN (SELECT customer_id FROM ${type} WHERE `
-    // }
+    if(ptype === 'CUSTOMER' && ptype != type) {
+        prefix = `id IN (SELECT customer_id FROM ${table} WHERE `
+    } else if(ptype === 'ORDER' && ptype != type) {
+        prefix = `id IN (SELECT order_id FROM ${table} WHERE `
+    } else if(ptype === 'PRODUCT' && ptype != type) {
+        prefix = `id IN (SELECT product_id FROM ${table} WHERE `
+    } else if(ptype === 'FULFILLMENT' && ptype != type) {
+        prefix = `id IN (SELECT fulfillment_id FROM ${table} WHERE `
+    } else if(typeof ptype === 'undefined' || ptype === type) {
+        prefix = `id IN (SELECT id FROM ${table} WHERE `
+        // if(type === 'CUSTOMER') {
+        //     prefix = `id IN (SELECT customer_id FROM ${table} WHERE `
+        // } else if(type === 'ORDER') {
+        //     prefix = `id IN (SELECT order_id FROM ${table} WHERE `
+        // } else if(type === 'FULFILLMENT') {
+        //     prefix = `id IN (SELECT fulfillment_id FROM ${table} WHERE `
+        // } else if(type === 'PRODUCT') {
+        //     prefix = `id IN (SELECT product_id FROM ${table} WHERE `
+        // }
+    } else {
+        return;
+    }
+
     let query = ''
     if (dataType === 'numeric') {
         if (filterType === 'equal_to') {
--- routes/whereClause.js
+++ routes/whereClause.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { whereClause } = require('../filters.js')
+const PostgresqlDb = require('../db')
 // const { setupWorkspace } = require('../controller/DataManager/Setup')
 // const { update } = require("../controller/ShopifyManager/Webhooks/index");
 // const {CUSTOMER_TABLE_NAME, ORDER_TABLE_NAME} = require("../controller/DataManager/helper");
@@ -10,7 +11,13 @@ const { whereClause } = require('../filters.js')
 
 router.post('/whereClause', async (req, res) => {
     const details = req.body
-    response = whereClause(details.filters, '')
-    // console.log(response)
-    res.status(200).send( { status: true, message: "successful", data: response } )
+    // const { 'x-workspace-id': workspaceId } = req.headers
+    let query = `SELECT * FROM ${details.tableName}333 WHERE `
+    query += `${whereClause()};`
+    console.log(query)
+    const response = await PostgresqlDb.query(query)
+    console.log(response)
+    res.status(200).send( { status: true, message: "successful"} )
 })
+
+module.exports = router
