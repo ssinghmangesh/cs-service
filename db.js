4241fb4d026edf7757acd4abbf597ccb0b589e45
Devashish Gupta <Reptile2503@users.noreply.github.com>
2021-06-09 12:43:01 +0530

    setup workspace and synced with shopify

--- db.js
+++ db.js
@@ -7,11 +7,11 @@ const pgConfig = () => {
     
     const env = 'local'
     return {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'cs-service', 
-        password: '25032000',
-        port: 5432
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DATABASE, 
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.POSTGRES_PORT
 	}
 
 }
--- routes/authShopifyManager.js
+++ routes/authShopifyManager.js
@@ -10,6 +10,8 @@ const nonce = require('nonce')();
 const querystring = require('querystring');
 const axios = require('axios');
 const { addWorkspace, fetchWorkspace, fetchUser, addUser, addUserToWorkspace } = require("../controller/UserManager");
+const { setupWorkspace } = require('../controller/DataManager/Setup')
+const { syncAll } = require("../controller/ShopifyManager/index");
 
 const shopifyApiPublicKey = 'eb6b044f4a8cf434a8100f85cac58205';
 const shopifyApiSecretKey = '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';
@@ -92,6 +94,7 @@ router.get('/callback', async (req, res) => {
         // console.log("shopData: ", shopData.data.shop)
 
         const fetchedWorkspace = await fetchWorkspace({ workspace_id: shopData.data.shop.id })
+        console.log(fetchedWorkspace);
         if (Object.keys(fetchedWorkspace).length === 0) {
             const workspace = {
                 workspace_id: shopData.data.shop.id,
@@ -103,6 +106,15 @@ router.get('/callback', async (req, res) => {
                 updated_at: Date.now()
             }
             await addWorkspace(workspace);
+            console.log('workspace added');
+            await setupWorkspace(workspace.workspace_id);
+            console.log('workspace setup done');
+            await syncAll({ 
+                shopName: workspace.shop_name, 
+                accessToken: workspace.access_token,  
+                limit: 50, 
+                workspaceId: workspace.workspace_id 
+            });
         }
 
         let flag = false;
