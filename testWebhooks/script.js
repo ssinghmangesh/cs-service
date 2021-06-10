cb5290cdeb625e67ec807cc190f46f521d26247e
Devashish Gupta <Reptile2503@users.noreply.github.com>
2021-06-10 13:50:01 +0530

    #

--- testWebhooks/script.js
+++ testWebhooks/script.js
@@ -1,25 +1,25 @@
 const axios = require('axios')
 
-const deleteWebhooks = async () => {
+const deleteScripts = async () => {
     res = await axios({
         method: 'GET',
-        url: 'https://indian-dress-cart.myshopify.com/admin/api/2021-04/webhooks.json',
+        url: 'https://indian-dress-cart.myshopify.com/admin/api/2021-04/script_tags.json',
         headers:  {
             'X-Shopify-Access-Token': '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',
         },
     })
-    for(let i=0; i < res.data.webhooks.length; i++){
-        const id = res.data.webhooks[i].id
+    for(let i=0; i < res.data.script_tags.length; i++){
+        const id = res.data.script_tags[i].id
         await axios({
             method: 'DELETE',
-            url: `https://indian-dress-cart.myshopify.com/admin/api/2021-04/webhooks/${id}.json`,
+            url: `https://indian-dress-cart.myshopify.com/admin/api/2021-04/script_tags/${id}.json`,
             headers:  {
                 'X-Shopify-Access-Token': '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',
             },
         })
     }
-    return 'webhooks deleted'
+    return 'scripts deleted'
 }
 
-deleteWebhooks()
+deleteScripts()
 .then(console.log)
