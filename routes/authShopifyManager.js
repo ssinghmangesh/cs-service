0eb8e4715b92657c7303fb6fdb8413418b702288
Devashish Gupta <Reptile2503@users.noreply.github.com>
2021-06-10 11:37:39 +0530

    updated app url

--- routes/authShopifyManager.js
+++ routes/authShopifyManager.js
@@ -17,7 +17,7 @@ const { createWebhooks } = require("../controller/ShopifyManager/Webhooks/index"
 const shopifyApiPublicKey = 'eb6b044f4a8cf434a8100f85cac58205';
 const shopifyApiSecretKey = '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';
 const scopes = 'read_products, read_product_listings, read_customers, read_orders, read_script_tags, write_script_tags, read_checkouts, read_draft_orders, read_price_rules, read_fulfillments, read_assigned_fulfillment_orders, read_content'
-const appUrl = 'http://localhost:3000';
+const appUrl = 'https://custom-segment-service.herokuapp.com';
 
 
 ///////////// Helper Functions /////////////
