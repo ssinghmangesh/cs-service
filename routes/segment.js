f8eeb00edcd48e600ff6fa445bff8d33c8deed52
Devashish Gupta <Reptile2503@users.noreply.github.com>
2021-07-02 12:07:15 +0530

    added route to delete segment

--- routes/segment.js
+++ routes/segment.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { addSegment, getSegments } = require('../controller/segment');
+const { addSegment, getSegments, deleteSegment } = require('../controller/segment');
 
 router.post('/segment/add', async (req, res) => {
     const { 'x-workspace-id': workspaceId } = req.headers
@@ -12,6 +12,12 @@ router.get('/segment/get', async (req, res) => {
     const { 'x-workspace-id': workspaceId } = req.headers
     const response = await getSegments(workspaceId)
     res.status(200).send( { status: true, message: "successful", segments: response.Items } )
+})
+
+router.post('/segment/delete', async (req, res) => {
+    const { 'x-workspace-id': workspaceId } = req.headers
+    await deleteSegment(workspaceId, req.body.segmentId)
+    res.status(200).send( { status: true, message: "successful" } )
 })
 
 module.exports = router
