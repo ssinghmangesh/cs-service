d07eb2d0f16ae5ab58b1f978da7f945ca28b780c
ssinghmangesh <ssingh.mangesh@gmail.com>
2021-07-20 02:38:31 +0530

    aded cors

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const port = 3000;
 const dotenv = require("dotenv").config();
 
 app.use(cors({
-    credentials: true, 
-    origin: true}
-));
+        credentials: true, 
+        origin: true
+    })
+);
 app.options('*', cors());
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.use(express.urlencoded({
     extended: true
