a945a3ea7a1b6758af087ef3c15a31617d2aa7be
ssinghmangesh <ssingh.mangesh@gmail.com>
2021-07-20 02:21:59 +0530

    aded cors

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const cors = require("cors");
 const port = 3000;
 const dotenv = require("dotenv").config();
 
-var corsOptions = {
-    origin: 'https://app.customsegment.com',
-    optionsSuccessStatus: 200 
-  }
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.urlencoded({
     extended: true
