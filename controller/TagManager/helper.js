4987cb6352bbc199337e70e458f4aef1fb7b0666
Devashish-25 <Devashish-25@users.noreply.github.com>
2021-07-30 11:41:35 +0530

    modified shopify scopes

---
+++ controller/TagManager/helper.js
@@ -0,0 +1,105 @@
+const { default: axios } = require("axios")
+const { whereClause } = require("../../filters")
+const PostgresqlDb = require('./../../db')
+
+const LIMIT = (limit) => {
+    if(limit){
+        return `LIMIT ${limit}`
+    } else {
+        return ''
+    }
+}
+
+const ORDER_BY = (orderBykey, orderByDirection) => {
+    if(orderBykey) {
+        if(orderByDirection) {
+            return `ORDER BY ${orderBykey} ${orderByDirection}`
+        } else {
+            return `ORDER BY ${orderBykey} asc`
+        }
+    } else {
+        return ''
+    }
+}
+
+const abstractData = (response, type) => {
+    //extract only rows
+    let res = null
+    if(type === "single") {
+        res = response.rows[0]
+    } else {
+        res = response.rows
+    }
+    return res
+}
+const WHERE_CLAUSE = (table, startdate, enddate) => {
+    if(startdate && enddate) {
+        let col = 'created_at'
+        if(table === 'inventorylevel') {
+            col = 'updated_at'
+        } else if(table === 'sentemail') {
+            col = 'sent_time'
+        }
+        return ` WHERE ${col} >= '${startdate}' AND ${col} <= '${enddate}'`
+    }
+    else return ''
+}
+
+const getwc = (datequery, wc1) => {
+    if(datequery) {
+        if(wc1) {
+            return datequery + ' AND ' + wc1 + ' '
+        } else {
+            return datequery
+        }
+    } else if(wc1) {
+        return 'WHERE ' + wc1
+    } else {
+        return ''
+    }
+}
+
+async function table({table, workspaceId, startdate, enddate, orderBykey, orderByDirection, limit, skipRowby = 0, filters = {}}) {
+    let wc = ``
+    // console.log('table filters: ', filters)
+    if(Object.entries(filters).length) {
+        wc = whereClause(filters, table, workspaceId);
+    }
+    let datequery = WHERE_CLAUSE(table, startdate, enddate)
+    wc = getwc(datequery, wc)
+    let query = ``
+    query = `
+        SELECT id FROM ${table}${workspaceId}
+        ${wc}
+        ${ORDER_BY(orderBykey, orderByDirection)}
+        ${LIMIT(limit)} 
+        OFFSET ${skipRowby};`
+    // console.log('@@@@@@@@@', query)
+    return abstractData(await PostgresqlDb.query(query));
+}
+
+const updateCustomer =  (shopName, accessToken, id, tags) => {
+    return axios.put(`https://${shopName}/admin/api/2021-07/customers/${id}.json`,{
+        customer: { id, tags }
+    }, {
+        headers: {
+            'X-Shopify-Access-Token': accessToken,
+        }
+    })
+}
+
+const updateOrder = (shopName, accessToken, id, tags) => {
+    return axios.put(`https://${shopName}/admin/api/2021-07/orders/${id}.json`,{
+        order: { id, tags }
+    }, {
+        headers: {
+            'X-Shopify-Access-Token': accessToken,
+        }
+    })
+}
+
+module.exports = {
+    table,
+    updateCustomer,
+    updateOrder,
+}
--- controller/TagManager/index.js
+++ controller/TagManager/index.js
@@ -1,4 +1,6 @@
 const { insert, query, del } = require("../../aws")
+const { fetchWorkspace } = require("../UserManager")
+const { table, updateCustomer, updateOrder } = require("./helper")
 
 const addTag = async (workspaceId, data) => {
     try{
@@ -48,6 +50,67 @@ const deleteTag = async (workspaceId, { tag_id }) => {
     }
     return await del(params);
 }
+
+const customerTags = async (tags, Item, workspaceId) => {
+    const customers = {}
+    for( let i=0; i<tags.length; i++){
+        const res = await table({table: 'customeraggregate', workspaceId, filters: tags[i].filters})
+        res.forEach(customer => {
+            customers[customer.id] = customers[customer.id] ? customers[customer.id]+', '+tags[i].then : tags[i].then;
+        })
+        // console.log(res);
+    }
+    // console.log(customers);
+    // console.log(tags);
+    const promises = Object.keys(customers).map(key => updateCustomer(Item.shop_name, Item.access_token, key, customers[key]))
+    await Promise.all(promises)
+    return true
+}
+
+const orderTags = async (tags, Item, workspaceId) => {
+    const orders = {}
+    for( let i=0; i<tags.length; i++){
+        const res = await table({table: 'order', workspaceId, filters: tags[i].filters})
+        console.log(res);
+        res.forEach(order => {
+            orders[order.id] = orders[order.id] ? orders[order.id]+', '+tags[i].then : tags[i].then;
+        })
+        // console.log(res);
+    }
+    const promises = Object.keys(orders).map(key => updateOrder(Item.shop_name, Item.access_token, key, orders[key]))
+    await Promise.all(promises)
+    return true;
+}
+
+const applyTags = async (workspaceId, {type, trigger}) => {
+    const params = {
+        TableName: 'Tags',
+        KeyConditionExpression: '#workspaceId = :workspaceId',
+        ExpressionAttributeNames: {
+            '#workspaceId': 'workspace_id',
+        },
+        ExpressionAttributeValues: {
+            ':workspaceId': workspaceId,
+        }
+    }
+    const { Item } = await fetchWorkspace({workspace_id: workspaceId})
+    // console.log(Item);
+    const res = await query(params)
+    const tags = res.Items.filter(item => (item.type === type && item.trigger_points.includes(trigger)));
+    switch(type) {
+        case 'customers':
+            await customerTags(tags, Item, workspaceId);
+            break;
+        case 'orders':
+            await orderTags(tags, Item, workspaceId);
+            break;
+        default:
+            break;
+    }
+    console.log('done');
+}
+
+// applyTags(56788582584, { type: 'orders', trigger: 'orderRefunded' });
 
 module.exports = {
     addTag,
